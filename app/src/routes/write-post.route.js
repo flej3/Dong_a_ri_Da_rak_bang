b3e664e5cdfb96b386573631d94ea45d0a419747
maengseohee <82189434+maengseohee@users.noreply.github.com>
2024-03-25 18:24:03 +0900

    게시글 작성 폼 수정, 로그인 여부로 게시글 작성 접근 등.. db에 저장 후 작업이랑 게시글 작성 후 작업 안했습니다.

--- app/src/routes/write-post.route.js
+++ app/src/routes/write-post.route.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const {createPost} =require("./../controllers/writePost.ctrl")
 
-router.get("/write-post", (req, res) => {
+// 로그인 상태를 확인하는 함수
+function checkAuth(req, res, next) {
+  // 쿠키에서 accessToken 가져오기
+  const accessToken = req.cookies.accessToken;
+
+  if (accessToken) {
+    next();
+  } else {
+    res.redirect("/pages-login");
+  }
+}
+
+router.get("/write-post", checkAuth, (req, res) => {
   res.render("write-post");
 });
+
+router.post('/postData', checkAuth, createPost);
 
 module.exports = router;
 
--- app/src/views/assets/js/post.js
+++ app/src/views/assets/js/post.js
@@ -1,3 +1,40 @@
-document.getElementById("writePostBtn").addEventListener("click", () => {
-    window.location.href = '/write-post';
-});
+function submitPostData() {
+    const postingData = {
+        club_name: document.getElementById('dongariName').value,
+        content: document.getElementById('introduction').value,
+        recruit_num: document.getElementById('recruitNumber').value,
+        dead_day: document.getElementById('recruitDeadline').value,
+        image_route: document.getElementById('postImageFile').value,
+    };
+
+    // 서버에 데이터 전송
+    fetch('/postData', {
+        method: 'POST',
+        body: JSON.stringify(postingData),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('네트워크 응답이 올바르지 않습니다.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        if (!data.success) {
+            alert('게시글 작성에 실패하였습니다.');
+            return;
+        }
+        alert('게시글 등록에 성공하였습니다.');
+        window.location.href = "/index";
+    })
+    .catch(error => {
+        console.error('오류 발생:', error.message);
+        alert('게시글 작성에 실패하였습니다.');
+    });
+}
+
+const completePostBtn = document.getElementById('completePostBtn');
+completePostBtn.addEventListener('click', submitPostData);

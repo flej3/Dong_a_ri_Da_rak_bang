1b54ea6806b33a257864ae319afec2b7b393fb58
maengseohee <82189434+maengseohee@users.noreply.github.com>
2024-04-14 21:59:22 +0900

    서비스 이용약관, 피드백 반영

--- app/src/views/assets/js/toggleTermsAgreement.js
+++ app/src/views/assets/js/toggleTermsAgreement.js
@@ -8,43 +8,32 @@ function toggleTermsModal() {
     }
 }
 
-// 이용약관 텍스트 클릭 이벤트 핸들러 추가
-document.getElementById('termsCheckboxWrapper').addEventListener('click', function() {
-    toggleTermsModal();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('acceptTerms').addEventListener('change', function() {
+        var termsModal = document.getElementById('termsModal');
+        if (termsModal) {
+            termsModal.style.display = this.checked ? 'block' : 'none';
+        }
+    });
 });
 
-// 동의합니다 체크박스 클릭 이벤트 핸들러 추가
-document.getElementById('agreementCheckbox').addEventListener('click', function() {
-    var acceptTerms = document.getElementById('acceptTerms');
-    acceptTerms.checked = this.checked;
-});
 
-// 확인 버튼 클릭 시 이용약관 닫기 및 이용약관 동의 여부 확인
+// 동의 버튼 클릭 시 이용약관 체크
 function confirmAgreement() {
     var acceptTerms = document.getElementById('acceptTerms');
     var modal = document.getElementById('termsModal');
 
     if (acceptTerms.checked) {
-        var agreementCheckbox = document.getElementById('agreementCheckbox');
-        agreementCheckbox.checked = true;
         modal.style.display = 'none';
     } else {
         alert("이용약관에 동의해주세요.");
     }
 }
 
-// 이용약관 동의 여부 확인 후 확인 버튼 활성화/비활성화 및 이용약관(보기) 체크 동기화
-function checkAgreement() {
-    var acceptTerms = document.getElementById('acceptTerms');
-    var confirmButton = document.getElementById('confirmButton');
-    var agreementCheckbox = document.getElementById('agreementCheckbox');
-
-    confirmButton.disabled = !acceptTerms.checked;
-    agreementCheckbox.checked = acceptTerms.checked;
-}
-
 // 이용약관 닫기
 function closeModal() {
     var modal = document.getElementById('termsModal');
+
+    // 모달이 닫힐 때는 체크 여부가 변경되지 않도록 수정
     modal.style.display = 'none';
 }

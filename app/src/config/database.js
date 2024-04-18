6ff8b25c0c77314600091978941dcc934ab83abd
김보석 <flej3@synapsoft.co.kr>
2024-04-18 16:36:45 +0900

    동아리 가입 신청에 대한 club 페이지에서 가입 신청 현황에서 핸들링이 가능합니다

--- app/src/config/database.js
+++ app/src/config/database.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 50,
+  connectionLimit: 100,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
